157a6e87890bfe96b79969cae77d350a766321ab
jonathanTorf <jonatantorf@gmail.com>
2026-02-03 22:00:38 +0200

    Update miniArcade.ino

--- miniArcade/miniArcade.ino
+++ miniArcade/miniArcade.ino
@@ -5,15 +5,18 @@
 
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 
-const int xPin = A0;
-const int yPin = A1;
+int xPin = A0;
+int yPin = A1;
 const int swPin = 2;
+int buzzerPin = 12;
 
 int gameSelection = 0;
+const int gameAmt = 1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(swPin, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
   matrix.begin(0x70);   //default I2C address
   matrix.setBrightness(2);
   matrix.setRotation(1);
@@ -32,10 +35,12 @@ void loop() {
   Serial.println("");
 
   if (xVal > 70) {   //if the joystick was moved far enoth to the left
+    if (gameSelection != 0) tone(buzzerPin, 500); //play a sound only if the first game isnt already selected
     gameSelection--; //move to teh previous game in the list
     selectScreen();
   }
   else if (xVal < 30) { //if the joystick was moved far enoth to the right
+    if (gameSelection != gameAmt) tone(buzzerPin, 500); //play a sound only if the last game isnt already selected
     gameSelection++;    //move to teh next game in the list
     selectScreen();
   }
@@ -45,6 +50,7 @@ void loop() {
   }
   
   delay(200);
+  noTone(buzzerPin); //stop all sound
 }
 
 void selectScreen() {
@@ -53,7 +59,7 @@ void selectScreen() {
     matrix.clear();                     //clear the screen(it would have been another games icon but thers currantly only snake
     matrix.writeDisplay();
   }
-  gameSelection = constrain(gameSelection, 0, 1); //keep gameSelection between 0 and 1
+  gameSelection = constrain(gameSelection, 0, gameAmt); //keep gameSelection between 0 and the number of games there are
 }
 
 /*const uint8_t gameOverBitmap[] PROGMEM = {
